a0853f192485b69e0ad122fc31f1915faadb3dab
antunes33 <feliperatko@gmail.com>
2023-09-05 08:50:41 -0300

    Regra negocial ChatGPT

--- src/main/java/br/com/open/ai/cecg/OpenAI/http/impl/OpenAIControllerImpl.java
+++ src/main/java/br/com/open/ai/cecg/OpenAI/http/impl/OpenAIControllerImpl.java
@@ -2,19 +2,19 @@ package br.com.open.ai.cecg.OpenAI.http.impl;
 
 import br.com.open.ai.cecg.OpenAI.http.OpenAIController;
 import br.com.open.ai.cecg.OpenAI.http.data.request.ScienceFictionRequestDTO;
+import br.com.open.ai.cecg.OpenAI.service.CallOpenAiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/openai-cecg")
-public class OpenAIControllerImpl implements OpenAIController {
+public record OpenAIControllerImpl(CallOpenAiService service) implements OpenAIController {
+
     @Override
     @PostMapping(value = "/science-fiction")
     @ResponseStatus(HttpStatus.OK)
-    public String generateScienceFiction(ScienceFictionRequestDTO requestDTO) {
-        return null;
+    public String generateScienceFiction(@RequestBody ScienceFictionRequestDTO requestDTO) {
+        return service.generateScienceFaction(requestDTO);
     }
 }
